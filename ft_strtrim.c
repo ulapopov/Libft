cba76795f6808d5930e3084987db1635927213cc
ulapopov <57166039+ulapopov@users.noreply.github.com>
2020-02-23 22:46:40 -0800

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -16,7 +16,7 @@
 
 char	*ft_strtrim(char const *s)
 {
-	int		i;
+/*	int		i;
 	char	*copy;
 	int		len;
 	int		size;
@@ -33,4 +33,28 @@ char	*ft_strtrim(char const *s)
 	ft_strncpy(copy, (char*)s + i, size);
 	copy[size] = '\0';
 	return (copy);
+*/
+		int		i;
+	int		len;
+	char	*str;
+
+	if (s == NULL)
+		return (NULL);
+	len = ft_strlen(s);
+	while (s[len - 1] == ' ' || s[len - 1] == '\t' || s[len - 1] == '\n')
+		len--;
+	i = -1;
+	while (s[++i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		len--;
+	if (len <= 0)
+		len = 0;
+	str = (char*)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	s += i;
+	i = -1;
+	while (++i < len)
+		str[i] = *s++;
+	str[i] = '\0';
+	return (str);
 }
