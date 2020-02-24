f4ec14da054b075ae20961d1a19f8447961ebc40
ulapopov <57166039+ulapopov@users.noreply.github.com>
2020-02-23 22:43:41 -0800

    Update ft_strnequ.c

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -14,7 +14,25 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (!ft_strncmp(s1, s2, n))
+	/*if (!ft_strncmp(s1, s2, n))
 		return (1);
+	return (0);
+	*/
+		int		i;
+
+	if (!n)
+		return (1);
+	if (s1 && s2 && n)
+	{
+		i = 0;
+		while (n > 0)
+		{
+			if (s1[i] != s2[i])
+				return (0);
+			i++;
+			n--;
+		}
+		return (1);
+	}
 	return (0);
 }
