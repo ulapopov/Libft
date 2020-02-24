09e1a0cf7aa55ef1a1822760d5ef31f34383082f
ulapopov <57166039+ulapopov@users.noreply.github.com>
2020-02-23 22:47:49 -0800

    Update ft_strsplit.c

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -13,7 +13,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "libft.h"
-
+/*
 static int		word_count(const char *s, char c)
 {
 	int			i;
@@ -55,4 +55,69 @@ char			**ft_strsplit(char const *s, char c)
 		i++;
 	}
 	return (arr);
+}
+*/
+static int		ft_nbw(const char *str, char c)
+{
+	int word;
+
+	word = 0;
+	if (*str != c && *str)
+	{
+		str++;
+		word++;
+	}
+	while (*str)
+	{
+		while (*str == c)
+		{
+			str++;
+			if (*str != c && *str)
+				word++;
+		}
+		str++;
+	}
+	return (word);
+}
+
+static int		ft_ln(const char *str, char c)
+{
+	int count;
+
+	count = 0;
+	while (*str != c && *str)
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	int		j;
+	int		i;
+	char	**spt;
+
+	j = 0;
+	i = 0;
+	if (!s || (!(spt = (char **)malloc(sizeof(char *) * (ft_nbw(s, c) + 1)))))
+		return (NULL);
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s != c && *s)
+		{
+			if (!(spt[j] = (char *)malloc(sizeof(char) * (ft_ln(s, c) + 1))))
+				return (NULL);
+			while (*s && *s != c)
+				spt[j][i++] = (char)*s++;
+			spt[j][i] = '\0';
+			j++;
+			i = 0;
+		}
+	}
+	spt[j] = NULL;
+	return (spt);
 }
