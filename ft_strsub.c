7362ce09d929ada6c2ec2af24c94977af0d1ae80
ulapopov <57166039+ulapopov@users.noreply.github.com>
2020-02-23 22:45:44 -0800

    Update ft_strsub.c

--- ft_strsub.c
+++ ft_strsub.c
@@ -15,7 +15,7 @@
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-	char			*str;
+/*	char			*str;
 	unsigned int	i;
 
 	str = (char*)malloc(len);
@@ -28,4 +28,23 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 		i++;
 	}
 	return (str);
+	*/
+		char	*result;
+	size_t	i;
+
+	if (s == NULL)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (NULL);
+	if ((result = (char *)malloc(sizeof(char) * (len + 1))) == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start] != '\0')
+	{
+		result[i] = s[start];
+		start++;
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
 }
